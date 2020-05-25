a4d9030def38b51b0bf180c84f3c9e927be65c86
Nour Balaha <nourbalaha0@gmail.com>
2020-05-26 02:09:51 +0900

    installed node-sass and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+
+import About from "./pages/About/About.page";
+import Portfolio from "./pages/Portfolio/Portfolio.page";
 
 function App() {
   return (
@@ -10,6 +14,13 @@ function App() {
           <span>protfolio</span>
         </nav>
       </header>
+      <main className="main">
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+      </Switch>
+    </main>
     </div>
   );
 }
