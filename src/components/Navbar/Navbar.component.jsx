175e4f56b9e43eb51266da1e36bdd5d4902257f3
Nour Balaha <nourbalaha0@gmail.com>
2020-05-27 02:04:08 +0900

    created navbar and styled it

---
+++ src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Navbar.style.scss";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+          <span className="nav-item">about</span>
+          <span className="nav-item">protfolio</span>
+          <span className="nav-item">contact</span>
+        </nav>
+    )
+}
