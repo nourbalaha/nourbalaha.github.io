ce221edb06c2f9b0913cb9d67be749283c876804
Nour Balaha <nourbalaha0@gmail.com>
2020-05-30 02:13:08 +0900

    added fonts to index.js and index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import './fonts/NeoSansPro-Regular.ttf'
+import './fonts/Raleway-Regular.ttf'
+import './fonts/ProximaNova-Regular.otf'
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
