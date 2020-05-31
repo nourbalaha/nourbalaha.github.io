98e13292c0a7376bf9991edf658f9fb96fa9011e
Nour Balaha <nourbalaha0@gmail.com>
2020-05-31 11:52:11 +0900

    wip Portfolio page

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import './Portfolio.style.scss'
+
+import ProjectCard from '../../components/ProjectCard/ProjectCard.component'
+
 export default function Portfolio() {
     return (
-        <div>
-            About
+        <div className='portfolio-page'>
+            <ProjectCard />
         </div>
     )
 }
