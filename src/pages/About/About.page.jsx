6b4eef28979338effea3fafed567d29f50f83717
Nour Balaha <nourbalaha0@gmail.com>
2020-05-31 11:49:43 +0900

    wip About page

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import "./About.style.scss";
+
+import nour from "../../assets/IMG_9377.jpg";
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className="about-page">
+            <div className="left">
+                <h2>about me</h2>
+                <h3>I'm a full stack web developer based in Tokyo, Japan.</h3>
+                <p>I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</p>
+            </div>
+            <div className="right">
+                <img className="about-image" src={nour} alt="Nour" />
+            </div>
         </div>
     )
 }
