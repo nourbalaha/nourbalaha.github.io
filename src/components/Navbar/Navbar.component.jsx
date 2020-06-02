6463cce2a85a43b7abdfabe6caecc087647deb4e
Nour Balaha <nourbalaha0@gmail.com>
2020-06-02 16:20:45 +0900

    adding functionality to navbar icon and items

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 
 import "./Navbar.style.scss";
 
 import logo from "../../assets/Logo.png";
 
-export default function Navbar() {
+function Navbar({ history }) {
     return (
         <nav className="navbar">
-          <img className="nav-img" src={logo} alt="logo" />
+          <img className="nav-img" onClick={()=>history.push("/")} src={logo} alt="logo" />
           <div className="nav-item-container">
-            <span className="nav-item">about</span>
-            <span className="nav-item">protfolio</span>
-            <span className="nav-item">contact</span>
+            <span className="nav-item" onClick={()=>history.push("/about")}>about</span>
+            <span className="nav-item" onClick={()=>history.push("/portfolio")}>protfolio</span>
+            <span className="nav-item" onClick={()=>history.push("/contact")}>contact</span>
           </div>
         </nav>
     )
 }
+
+export default withRouter(Navbar);
