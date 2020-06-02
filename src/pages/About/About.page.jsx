9ae46387ceff649946fc8bd468b782d2f7282341
Nour Balaha <nourbalaha0@gmail.com>
2020-06-02 17:51:09 +0900

    changed headers to spans and modified styles

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -8,9 +8,9 @@ export default function About() {
     return (
         <div className="about-page">
             <div className="left">
-                <h2>about me</h2>
-                <h3>I'm a full stack web developer based in Tokyo, Japan.</h3>
-                <p>I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</p>
+                <span className="left-header">about me</span>
+                <span className="left-title">I'm a full stack web developer based in Tokyo, Japan.</span>
+                <span className="left-text">I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</span>
             </div>
             <div className="right">
                 <img className="about-image" src={nour} alt="Nour" />
