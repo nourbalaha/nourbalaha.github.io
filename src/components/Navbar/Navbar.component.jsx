1baaa043b895f89b0df0a8b70ece7ab182c63986
nourbalaha <nourbalaha0@gmail.com>
2020-06-02 15:00:34 +0900

    added nav logo to the navbar

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import "./Navbar.style.scss";
 
+import logo from "../../assets/Logo.png";
+
 export default function Navbar() {
     return (
         <nav className="navbar">
-          <span className="nav-item">about</span>
-          <span className="nav-item">protfolio</span>
-          <span className="nav-item">contact</span>
+          <img className="nav-img" src={logo} alt="logo" />
+          <div className="nav-item-container">
+            <span className="nav-item">about</span>
+            <span className="nav-item">protfolio</span>
+            <span className="nav-item">contact</span>
+          </div>
         </nav>
     )
 }
