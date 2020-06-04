685ebb0a53d9704a256178422a51743435154ba4
Nour Balaha <nourbalaha0@gmail.com>
2020-06-04 19:57:31 +0900

    added projects to portfolio page

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -4,10 +4,16 @@ import './Portfolio.style.scss'
 
 import ProjectCard from '../../components/ProjectCard/ProjectCard.component'
 
+import socialEyes from "../../assets/social-eyes.gif";
+import gamerStore from "../../assets/gamer-store.gif"
+import habbits from "../../assets/habbits.png"
+
 export default function Portfolio() {
     return (
         <div className='portfolio-page'>
-            <ProjectCard />
+            <ProjectCard name="Social Eyes" description="social app that allow you to connect with people all over the world." img={socialEyes} />
+            <ProjectCard name="Gamer Store" description="e-commerce online store for manage and selling video games." img={gamerStore} />
+            <ProjectCard name="Habbits" description="Mobile app to help you build good habbits." img={habbits} />
         </div>
     )
 }
