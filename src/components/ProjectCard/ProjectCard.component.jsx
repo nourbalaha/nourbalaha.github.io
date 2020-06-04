3f52a7f3b00cf904f585639bb5f2a67ed1704267
Nour Balaha <nourbalaha0@gmail.com>
2020-06-04 19:54:35 +0900

    edited and styled Project card component

--- src/components/ProjectCard/ProjectCard.component.jsx
+++ src/components/ProjectCard/ProjectCard.component.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 import "./ProjectCard.style.scss"
 
-import social from "../../assets/social-eyes.gif"
-
-export default function ProjectCard() {
+export default function ProjectCard({name, description, img}) {
     return (
         <div className="project-card">
-            <img className="project-card-image" src={social} alt="social-eyes" />
-            <h4 className="project-card-header">Social Eyes</h4>
-            <p className="project-card-text">social app that allow you to connect with people all over the world.</p>
+            <img className="project-card-image" src={img} alt={description} />
+            <h4 className="project-card-header">{name}</h4>
+            <p className="project-card-text">{description}</p>
+            <hr />
+            <div className="project-card-link-container">
+                <span className="project-card-link">github</span>
+                <span className="project-card-link">website</span>
+            </div>
         </div>
     )
 }
