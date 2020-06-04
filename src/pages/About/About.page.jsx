b8cc766a11177f744b992d168d989f8fec92a686
Nour Balaha <nourbalaha0@gmail.com>
2020-06-04 19:55:51 +0900

    edited and styled about page

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -8,9 +8,9 @@ export default function About() {
     return (
         <div className="about-page">
             <div className="left">
-                <span className="left-header">about me</span>
-                <span className="left-title">I'm a full stack web developer based in Tokyo, Japan.</span>
-                <span className="left-text">I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</span>
+                <h2 className="left-header">about me</h2>
+                <h4 className="left-title">I'm a full stack web developer based in Tokyo, Japan.</h4>
+                <p className="left-text">I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</p>
             </div>
             <div className="right">
                 <img className="about-image" src={nour} alt="Nour" />
