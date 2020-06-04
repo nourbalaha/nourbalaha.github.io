0b8aa92c3fc757f0a5f0cc7801ea14774c007f7e
Nour Balaha <nourbalaha0@gmail.com>
2020-06-05 00:25:16 +0900

    added links to project cards

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -11,9 +11,9 @@ import habbits from "../../assets/habbits.png"
 export default function Portfolio() {
     return (
         <div className='portfolio-page'>
-            <ProjectCard name="Social Eyes" description="social app that allow you to connect with people all over the world." img={socialEyes} />
-            <ProjectCard name="Gamer Store" description="e-commerce online store for manage and selling video games." img={gamerStore} />
-            <ProjectCard name="Habbits" description="Mobile app to help you build good habbits." img={habbits} />
+            <ProjectCard name="Social Eyes" description="social app that allow you to connect with people all over the world." img={socialEyes} github="https://github.com/nourbalaha/social-eyes" website="https://social-eyez.herokuapp.com/" />
+            <ProjectCard name="Gamer Store" description="e-commerce online store for manage and selling video games." img={gamerStore} github="https://github.com/nourbalaha/gamer-store" website="http://gamer-store.herokuapp.com/" />
+            <ProjectCard name="Habbits" description="Mobile app to help you build good habbits." img={habbits} github="https://github.com/cc5-team-blue/habbits" website="https://www.buildhabbits.today/" />
         </div>
     )
 }
