fd03fb0b04c5ba181b7255bea940bc3627b2b3f7
Nour Balaha <nourbalaha0@gmail.com>
2020-06-04 23:25:25 +0900

    added functionality to footer links

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -3,15 +3,27 @@ import React from 'react'
 import './Footer.style.scss'
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-        <div className="footer-item-container">
-          <span className="footer-item"><i class="fa fa-envelope"></i></span>
-          <span className="footer-item"><i class="fa fa-linkedin"></i></span>
-          <span className="footer-item"><i class="fa fa-twitter"></i></span>
-          <span className="footer-item"><i class="fa fa-github"></i></span>
-        </div>
-        <span>© 2020 Nour Balaha</span>
-        </footer>
-    )
+
+  const copyToClipboard = () => {
+    const el = document.createElement("textarea");
+    el.value="nourbalaha0@gmail.com";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert("email has been copied to the clipboard");
+  }
+
+  return (
+      <footer className="footer">
+      <div className="footer-item-container">
+        <span className="footer-item" onClick={copyToClipboard}><i class="fa fa-envelope"></i></span>
+        <span className="footer-item" onClick={()=>window.open("https://www.linkedin.com/in/nourbalaha0/", '_blank')}><i class="fa fa-linkedin"></i></span>
+        <span className="footer-item" onClick={()=>window.open("https://twitter.com/NourBalaha", '_blank')}><i class="fa fa-twitter"></i></span>
+        <span className="footer-item" onClick={()=>window.open("https://github.com/nourbalaha", '_blank')}><i class="fa fa-github"></i></span>
+      </div>
+      <span>© 2020 Nour Balaha</span>
+      </footer>
+  )
+  
 }
