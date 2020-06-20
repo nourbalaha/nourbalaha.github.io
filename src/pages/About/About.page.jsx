d3581a4e3ce864c3b610dcf36feab69c451eaeb8
Nour Balaha <nourbalaha0@gmail.com>
2020-06-21 08:55:51 +0900

    changed title in about page

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -9,7 +9,7 @@ export default function About() {
         <div className="about-page">
             <div className="left">
                 <h2 className="left-header">about me</h2>
-                <h4 className="left-title">I'm a full stack web developer based in Tokyo, Japan.</h4>
+                <h4 className="left-title">I'm a Software Engineer based in Tokyo, Japan.</h4>
                 <p className="left-text">I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</p>
             </div>
             <div className="right">
