9e15387ddf5728b714ca791aba32aa6bad46a9a5
Nour Balaha <nourbalaha0@gmail.com>
2020-06-26 22:46:58 +0900

    wip my skills section

--- src/pages/About/About.page.jsx
+++ src/pages/About/About.page.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-import "./About.style.scss";
+import './About.style.scss'
 
-import nour from "../../assets/profile.png";
+import nour from '../../assets/profile.png'
 
-export default function About() {
-    return (
-        <div className="about-page">
-            <div className="left">
-                <h2 className="left-header">about me</h2>
-                <h4 className="left-title">I'm a Software Engineer based in Tokyo, Japan.</h4>
-                <p className="left-text">I enjoy turning complex problems into simple ones. When I'm not coding, you'll find me learning Japanese, playing video games or working out in the park.</p>
-            </div>
-            <div className="right">
-                <img className="about-image" src={nour} alt="Nour" />
-            </div>
+import SkillsChart from "../../components/SkillsChart/SkillsChart.component"
+
+export default function About () {
+  return (
+    <div className='about-page'>
+      <div className='about-section'>
+        <div className='left'>
+          <h2 className='left-header'>about me</h2>
+          <h4 className='left-title'>
+            I'm a Software Engineer based in Tokyo, Japan.
+          </h4>
+          <p className='left-text'>
+            I enjoy turning complex problems into simple ones. When I'm not
+            coding, you'll find me learning Japanese, playing video games or
+            working out in the park.
+          </p>
         </div>
-    )
+        <div className='right'>
+          <img className='about-image' src={nour} alt='Nour' />
+        </div>
+      </div>
+      <div className='skills-section'>
+        <h2>My Skills</h2>
+        <SkillsChart />
+      </div>
+
+    </div>
+  )
 }
