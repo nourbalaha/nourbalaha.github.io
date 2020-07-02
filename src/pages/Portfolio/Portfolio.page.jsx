5d0325f881756634e26e13ba830d2bd0745fa384
Nour Balaha <nourbalaha0@gmail.com>
2020-07-02 21:53:24 +0900

    replaced habbits website link with linkedin link

--- src/pages/Portfolio/Portfolio.page.jsx
+++ src/pages/Portfolio/Portfolio.page.jsx
@@ -13,7 +13,7 @@ export default function Portfolio() {
         <div className='portfolio-page'>
             <ProjectCard name="Social Eyes" description="social app that allow you to connect with people all over the world." img={socialEyes} github="https://github.com/nourbalaha/social-eyes" website="https://social-eyez.herokuapp.com/" />
             <ProjectCard name="Gamer Store" description="e-commerce online store for manage and selling video games." img={gamerStore} github="https://github.com/nourbalaha/gamer-store" website="http://gamer-store.herokuapp.com/" />
-            <ProjectCard name="Habbits" description="Mobile app to help you build good habbits." img={habbits} github="https://github.com/cc5-team-blue/habbits" website="https://www.buildhabbits.today/" />
+            <ProjectCard name="Habbits" description="Mobile app to help you build good habbits." img={habbits} github="https://github.com/cc5-team-blue/habbits" website="https://github.com/cc5-team-blue/habbits" />
         </div>
     )
 }
