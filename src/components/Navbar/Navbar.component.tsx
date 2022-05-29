7183590001ce5974da3439b8d0ef062dab8ac736
nourbalaha <nourbalaha0@gmail.com>
2022-05-30 01:11:40 +0900

    feat: added resume to navbar + cleanup

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -5,32 +5,39 @@ import "./Navbar.style.scss";
 
 import logo from "../../assets/Logo.png";
 
-const Navbar: FC<any> = ({ history }) => {
+const Navbar: FC<any> = () => {
   const navigate = useNavigate();
 
   return (
-  <nav className="navbar">
-    <img
-      className="nav-img"
+    <nav className="navbar">
+      <img
+        className="nav-img"
         onClick={() => navigate("/")}
-      src={logo}
-      alt="logo"
-    />
-    <div className="nav-item-container">
+        src={logo}
+        alt="logo"
+      />
+      <div className="nav-item-container">
         <span className="nav-item" onClick={() => navigate("/about")}>
-        about
-      </span>
+          about
+        </span>
         <span className="nav-item" onClick={() => navigate("/experience")}>
           experience
         </span>
         <span className="nav-item" onClick={() => navigate("/portfolio")}>
-        protfolio
-      </span>
+          protfolio
+        </span>
         <span className="nav-item" onClick={() => navigate("/contact")}>
-        contact
-      </span>
-    </div>
-  </nav>
+          contact
+        </span>
+        <a
+          className="resume"
+          href={process.env.PUBLIC_URL + '/resume.pdf'}
+          download
+        >
+          resume
+        </a>
+      </div>
+    </nav>
   )
 };
 
